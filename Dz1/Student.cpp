0a045e95b1e4acd60318a254201c9b632d126665
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-05-25 17:27:22 +0500

    Change Student

--- Dz1/Student.cpp
+++ Dz1/Student.cpp
@@ -2,56 +2,20 @@
 
 Student::Student() {}
 
-/*Human::Human(std::string name, std::string surname, int age, int weight)
-	: name(name), surname(surname), age(age), weight(weight) {
-
-}*/
-
-void Student::setName(std::string name) {
-	person.setName(name);
-}
-std::string Student::getName() const {
-	return person.getName();
-}
-
-void Student::setSurname(std::string surname) {
-	person.setSurname(surname);
-}
-std::string Student::getSurname() const {
-	return person.getSurname();
-}
-
-void Student::setAge(int age) {
-	person.setAge(age);
-}
-int Student::getAge() const {
-	return person.getAge();
-}
-
-void Student::setWeight(int weight) {
-	person.setWeight(weight);
-}
-int Student::getWeight() const {
-	return person.getWeight();
-}
-
-void Student::setGrades(double grades) {
+Student::Student(Human person, double grades,
+	std::string group, std::string direction) {
+	person = person;
 	grades = grades;
-}
-double Student::getGrades() const {
-	return grades;
-}
-
-void Student::setGroup(std::string group) {
 	group = group;
-}
-std::string Student::getGroup() const {
-	return group;
-}
-
-void Student::setDirection(std::string direction) {
 	direction = direction;
 }
-std::string Student::getDirection() const {
-	return direction;
+
+void Student::setPerson(Human person) {
+	person = person;
 }
+
+Human Student::getPerson() const{
+	return person;
+}
+
+
--- Dz1/Student.h
+++ Dz1/Student.h
@@ -10,27 +10,9 @@ public:
 
 	Student(Human person, double grades,
 		std::string group, std::string direction);
-	
-	void setName(std::string name);
-	std::string getName() const;
 
-	void setSurname(std::string surname);
-	std::string getSurname() const;
-
-	void setAge(int age);
-	int getAge() const;
-
-	void setWeight(int weight);
-	int getWeight() const;
-
-	void setGrades(double grades);
-	double getGrades() const;
-
-	void setGroup(std::string group);
-	std::string getGroup() const;
-
-	void setDirection(std::string direction);
-	std::string getDirection() const;
+	void setPerson(Human person);
+	Human getPerson() const;
 
 private:
 	Human person;

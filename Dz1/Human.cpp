90743deffd4458843806ecca045214c6cbca47f0
Dima <you@example.com>
2022-06-29 21:22:17 +0500

    exception not complete

--- Dz1/Human.cpp
+++ Dz1/Human.cpp
@@ -1,5 +1,7 @@
 #include "Human.h"
 
+using namespace std;
+
 Human::Human():name("Sam"), surname("Carlos"), age(26), weight(75) {}
 
 Human::Human(std::string name, std::string surname, int age, int weight)
@@ -8,29 +10,41 @@ Human::Human(std::string name, std::string surname, int age, int weight)
 }
 
 void Human::setName(std::string name) {
+	if (name == "") {
+		throw std::invalid_argument("name cannot be empty");
+	}
 	this->name = name;
 }
-std::string Human::getName() const {
+std::string Human::getName() const noexcept{
 	return name;
 }
 
 void Human::setSurname(std::string surname) {
+	if (surname == "") {
+		throw std::invalid_argument("surname cannot be empty");
+	}
 	this->surname = surname;
 }
-std::string Human::getSurname() const {
+std::string Human::getSurname() const noexcept{
 	return surname;
 }
 
 void Human::setAge(int age) {
+	if (age <= 0) {
+		throw std::invalid_argument("age cannot be less than one");
+	}
 	this->age = age;
 }
-int Human::getAge() const {
+int Human::getAge() const noexcept{
 	return age;
 }
 
 void Human::setWeight(int weight) {
+	if (weight <= 0) {
+		throw std::invalid_argument("weight cannot be less than one");
+	}
 	this->weight = weight;
 }
-int Human::getWeight() const {
+int Human::getWeight() const noexcept{
 	return weight;
 }
